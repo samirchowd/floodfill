2d7d032f06824d4166404b8a6f88654fbcb33e71
samirchowd <samirchowd@gmail.com>
2021-01-04 13:58:23 -0500

    First pass script finished. Need to write test and debug

--- rewrite/flood.py
+++ rewrite/flood.py
@@ -3,9 +3,10 @@ import numpy as np
 
 class FloodFill():
 
-    def __init__(self, data, adj, bandpass=(300,7500)):
+    def __init__(self, data, adj, p = 1, bandpass=(300,7500)):
         self.data = data
         self.adj = adj
+        self.p = 1
         self.bandpass = bandpass
 
     def detect_spikes(self, weakMul=2, strongMul=4):
@@ -31,7 +32,7 @@ class FloodFill():
                     spk_wt = []
                     spk_loc = []
                     # Call flood_fill on strong crossing 
-                    spk_wt, spk_loc, res_bin = floodfill(self, t+1, i, weak, strong, spk_wt, spk_loc, res_bin)
+                    spk_wt, spk_loc, res_bin = floodfill(self, t+1, i, spk_wt, spk_loc, res_bin)
                     
                     # Calculate Voltage Weighted Spk Center 
                     spk_wt =  spk_center(spk_wt)
@@ -44,22 +45,38 @@ class FloodFill():
     def flood_fill(self, t, c, weak, strong, spk_wt, spk_loc, res_bin):
         
         # Validating data 
-        if validate_data(self, t, c, weak, res_bin):
+        if validate_data(self, t, c, self.weak, res_bin):
             return
         
         # Calulating psi value and appending it and the (t,c) to matricies 
         # BC: Check append memory usage 
-        spk_wt.append((t, psi(self, t, c, weak, strong)))
+        spk_wt.append((t, psi(self, t, c, self.weak, self.strong)))
+        spk_loc.append((t,c))
         
+        res_bin[t][c] = 1
+        
+        # TODO: Recursive Calls then switch to iterative
+        for tc in self.adj: 
+            if c == tc[0]: 
+                if not res_bin(t, tc[1]):
+                    spk_wt, spk_loc, res_bin = floodfill(self, t, tc[1], spk_wt, spk_loc, res_bin)
+                    
+        if not res_bin(t+1, c):
+            spk_wt, spk_loc, res_bin = floodfill(self, t+1, c, spk_wt, spk_loc, res_bin)
+        
+        if not res_bin(t-1, c):
+            spk_wt, spk_loc, res_bin = floodfill(self, t-1, c, spk_wt, spk_loc, res_bin)
+            
+        return spk_wt, spk_loc, res_bin 
         
     def psi(self, t, c, weak, strong):
         return min(((-self.data[t,c] - weak) / (strong - weak)), 1);
     
-    def spk_center(spk_wt):
-        x = []
-        y = [] 
-        for tc 
-    
+    def spk_center(self, spk_wt):
+        x = np.asarray([x[0] for x in spk_weights])
+        y = np.asarray([y[1] for y in spk_weights])
+        
+        return np.sum(np.power(x*y, self.p)) / np.sum(np.power(y, self.p))
         
     def cross_detect(self, strong):
         index = np.diff(np.sign(self.data-strong), axis=0) > 0
--- rewrite/test.py
+++ rewrite/test.py
@@ -1,2 +1,7 @@
-def square(x):
-    return x**2
+import flood 
+import numpy as np
+
+adj =  np.genfromtxt('adj.csv', delimiter=',')
+data = np.genfromtxt('data.csv', delimiter=',')
+
+print(type(data), type(adj))
